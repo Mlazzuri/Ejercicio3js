8ac7f27d4c5bff2222daebabfa0f71ec30f21eda
Mlazzuri <marianolazzurii@gmail.com>
2022-10-04 20:35:07 -0300

    Ejercicio 3

---
+++ ejercicio_3.js
@@ -0,0 +1,28 @@
+let Nombre ="Mariano"
+let Apellido = "Lazzuri"
+
+let estudiante = (Nombre + " "+ Apellido)
+console.log(estudiante)
+
+let estudianteMayus= (estudiante.toUpperCase())
+console.log(estudianteMayus)
+
+let estudianteMinus = (estudiante.toLowerCase())
+console.log(estudianteMinus)
+
+let nletras =(estudiante.length)
+console.log(nletras)
+
+let primeraLetra = (Nombre[0])
+console.log(primeraLetra)
+
+let UltimaLetra = (Apellido[6])
+console.log(UltimaLetra)
+
+let eliminarespacio = (estudiante.replace(" ",""))
+console.log(eliminarespacio)
+
+let contenido = (estudiante.includes("Mariano"))
+console.log(contenido)
+
+
